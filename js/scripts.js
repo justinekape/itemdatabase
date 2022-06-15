f7220082fb14d5fbccb198360d2c60d443d71710
justine <106638324+justinekape@users.noreply.github.com>
2022-06-15 13:02:13 -0400

    added new variable and objects to array

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,5 @@
-alert('Hello World');
-
-let favoriteFood = 'sushi';
-document.write(favoriteFood);
+let pokemonList = [
+  { name: 'pikachu', height: 0.4, type: 'electric'},
+  {name: 'charizard', height: 1.7, types: ['fire', 'flying']},
+  {name: 'nidoking', height: 1.4, types: ['ground', 'poison']},
+];
