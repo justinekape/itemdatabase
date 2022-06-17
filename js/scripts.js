14aacbca20fcdc8d64071380fabac0dc018c5702
justine <106638324+justinekape@users.noreply.github.com>
2022-06-16 21:19:58 -0400

    added for loop and conditionals

--- js/scripts.js
+++ js/scripts.js
@@ -3,3 +3,16 @@ let pokemonList = [
   {name: 'charizard', height: 1.7, types: ['fire', 'flying']},
   {name: 'nidoking', height: 1.4, types: ['ground', 'poison']},
 ];
+
+
+
+for (let i = 0; i < pokemonList.length; i++) {
+  document.write('<br>' + pokemonList[i].name + ' ' + 'height:'+ ' ' +  pokemonList[i].height + ' ');
+// question: would it be easier to add <br> in my js string and then formatting it through CSS?
+
+if (pokemonList[i].height > 1.5) {
+  document.write(' -i\'m big!');
+} else {
+  document.write('');
+}
+}
