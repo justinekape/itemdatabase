45e2914e481bd6337fc1e803fb7fee1b5c673a03
justine <106638324+justinekape@users.noreply.github.com>
2022-06-27 14:02:20 -0400

    added eventListener and showDetails
    deleted unnecessary comments

--- js/scripts.js
+++ js/scripts.js
@@ -12,11 +12,14 @@ let pokemonRepository = (function () {
     return myPokemonList;
   }
   function addListItem(pokemon){
-    let pokemonList = document.querySelector(".pokemon-list");
-    let listpokemon = document.createElement("li");
-    let button = document.createElement("button");
+    let pokemonList = document.querySelector('.pokemon-list');
+    let listpokemon = document.createElement('li');
+    let button = document.createElement('button');
+    button.addEventListener('click', function () {
+      showDetails(pokemon);
+    });
     button.innerText = pokemon.name;
-    button.classList.add("button-main");
+    button.classList.add('button-main');
     listpokemon.appendChild(button);
     pokemonList.appendChild(listpokemon);
   }
@@ -47,45 +50,3 @@ console.log(pokemonRepository.getAll());
 pokemonRepository.getAll().forEach(function (pokemon) {
   pokemonRepository.addListItem(pokemon);
 });
-
-
-// *** CODE FROM EXERCISE THAT I MODIFIED. TRIED IT AND WORKED, THE ONLY DIFFERENCE WAS THE forEach LOOP AND THE DOBULE QUOTATION
-
-// let pokemonRepository = (function () {
-//   let myPokemonList = [
-//   { name: 'pikachu', height: 0.4, type: 'electric'},
-//   {name: 'charizard', height: 1.7, types: ['fire', 'flying']},
-//   {name: 'nidoking', height: 1.4, types: ['ground', 'poison']},
-//   ];
-//
-//   function add(pokemon) {
-//     myPokemonList.push(pokemon);
-//   }
-//   function getAll() {
-//     return myPokemonList;
-//   }
-//   function addListItem(pokemon){
-//     let pokemonList = document.querySelector(".pokemon-list");
-//     let listpokemon = document.createElement("li");
-//     let button = document.createElement("button");
-//     button.innerText = pokemon.name;
-//     button.classList.add("button-main");
-//     listpokemon.appendChild(button);
-//     pokemonList.appendChild(listpokemon);
-//   }
-//   function showDetails(pokemon) {
-//     console.log(pokemon);
-//   }
-//
-//   return {
-//     add: add,
-//     getAll: getAll,
-//     addListItem: addListItem
-//   };
-// })();
-//
-// console.log(pokemonRepository.getAll());
-//
-// pokemonRepository.getAll().forEach(function (pokemon) {
-//   pokemonRepository.addListItem(pokemon);
-// });
